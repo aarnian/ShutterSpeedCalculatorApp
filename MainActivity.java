d1e871e5c87c1b1fc8c960c7445471fc833c2e17
aarnian <aarni.antti@gmail.com>
2021-04-11 12:21:17 +0300

    Update MainActivity.java
    Tulos ja clear-nappi

--- MainActivity.java
+++ MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     // K voi olla esim 1-3, suurempi = enemmän motion blur
     //int k = 1;
-    //Pixel pitch 1200D:ssä on 4,29µm
+    //Pixel pitch 1200D:ssä 4,29µm
     //float p = (float) 4.29;
 
 
@@ -28,79 +29,84 @@ public class MainActivity extends AppCompatActivity {
         //Aperture
         EditText t = findViewById(R.id.source);
         String input = t.getText().toString();
-        Float N = new Float(input);
+        float N;
+        if (input.matches("")) {
+            N = 0;
+        } else {
+            N = new  Float(input);
+        }
         Log.d("info", input);
 
         //Focal length
         EditText t2 = findViewById(R.id.source2);
         String input2 = t2.getText().toString();
-        Float f = new Float(input2);
+        float f;
+        if (input2.matches("")) {
+            f = 0;
+        } else {
+            f = new  Float(input2);
+        }
         Log.d("info", input2);
 
         //Min declination
         EditText t3 = findViewById(R.id.source3);
         String input3 = t3.getText().toString();
-        Float d = new Float(input3);
+        float d;
+        if (input3.matches("")) {
+            d = 0;
+        } else {
+            d = new  Float(input3);
+        }
         Log.d("info", input3);
 
         //Multiplier
         EditText t4 = findViewById(R.id.source4);
         String input4 = t4.getText().toString();
-        Float k = new Float(input4);
+        float k;
+        if (input4.matches("")) {
+            k = 0;
+        } else {
+            k = new  Float(input4);
+        }
         Log.d("info", input4);
 
         //Pixel pitch
         EditText t5 = findViewById(R.id.source5);
         String input5 = t5.getText().toString();
-        Float p = new Float(input5);
+        float p;
+        if (input5.matches("")) {
+            p = 0;
+        } else {
+            p = new  Float(input5);
+        }
         Log.d("info", input5);
 
+        //Funktio
         Double a = k * ((16.9 * N + 0.1 * f + 13.7 * p) / f * Math.cos(d));
-
+        Double rounded = Math.round(a * 10) / 10.0;
         System.out.println(a);
 
+        //Tulos
+        ((TextView)findViewById(R.id.output)).setText(rounded.toString() + " seconds");
 
-        //Ottaa syötetyt numerot ja tekee niistä arrayn
-
-        /*
-
-        EditText t = findViewById(R.id.source);
-        String input = t.getText().toString();
-        Float finput = new Float(input);
-        Log.d("info", input);
-
-        EditText t2 = findViewById(R.id.source2);
-        String input2 = t2.getText().toString();
-        Float finput2 = new Float(input2);
-        Log.d("info", input2);
-
-        EditText t3 = findViewById(R.id.source3);
-        String input3 = t3.getText().toString();
-        Float finput3 = new Float(input3);
-        Log.d("info", input3);
-
-        EditText t4 = findViewById(R.id.source4);
-        String input4 = t4.getText().toString();
-        Float finput4 = new Float(input4);
-        Log.d("info", input4);
-
-        EditText t5 = findViewById(R.id.source5);
-        String input5 = t5.getText().toString();
-        Float finput5 = new Float(input5);
-        Log.d("info", input5);
-
-        float[] nums = new float[5];
-        nums[0] = finput;
-        nums[1] = finput2;
-        nums[2] = finput3;
-        nums[3] = finput4;
-        nums[4] = finput5;
-
-        //System.out.println(Arrays.toString(nums));
-
-        return nums;
-
-        */
     }
+
+    public void clear(View v){
+        EditText t = findViewById(R.id.source);
+        EditText t2 = findViewById(R.id.source2);
+        EditText t3 = findViewById(R.id.source3);
+        EditText t4 = findViewById(R.id.source4);
+        EditText t5 = findViewById(R.id.source5);
+
+        t.setText("");
+        t2.setText("");
+        t3.setText("");
+        t4.setText("");
+        t5.setText("");
+    }
+
+
+
+
 }
 
